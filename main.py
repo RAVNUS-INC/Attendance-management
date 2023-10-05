9feec628de658ddac045386c1a5fee39e7ca6bd2
stopdragon <jiyong210@gmail.com>
2023-10-05 23:21:41 +0900

    check example

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
-import os, requests
-from dotenv import load_dotenv
-from datetime import datetime
+POST https://workplace.apigw.ntruss.com/timeclock/apigw/v1/company/5b8530ef-75fb-4bd1-aed4-7bba5a3f9c75/timeclock/check
 
-load_dotenv()
 
-# 출근
-POST https://workplace.apigw.ntruss.com/timeclock/apigw/v1/company/e065a8c6-2ab5-41a7-a4de-bf70c9a08142/timeclock/check
+/* 1회체크, 출퇴근 예시 */
+POST https://workplace.apigw.ntruss.com/timeclock/apigw/v1/company/f3cf9659-782c-4508-bef9-f34c93f446df/timeclock/check
 HHOST: workplace.apigw.ntruss.com
  Content-Type: application/json
- x-ncp-apigw-timestamp: datetime.datetime.now()
- x-ncp-apigw-api-key: os.environ.get("API-key")
- x-ncp-iam-access-key: os.environ.get("access-key")
- x-ncp-apigw-signature-v1: os.environ.get("secret-key")
+ x-ncp-apigw-timestamp: 1505290625682
+ x-ncp-apigw-api-key: DUxjP4HYTP8V7khBkgywdDHQjKuaYWGrppHKpvdv
+ x-ncp-iam-access-key: D78BB444D6D3C84CA38A
+ x-ncp-apigw-signature-v1: WTPItrmMIfLUk/UyUIyoQbA/z5hq9o3G8eQMolUzTEo=
+{
+  "locale": "ko_KR",
+  "commuteType": "onoff",
+  "companyId": "fe5f8382-aae3-4746-8d6e-f9d0545c5a9b",
+  "emailId": "1year1@naver.com",
+  "basicYmd": "20190918",
+  "onYmdhm": "201909181559",
+  "breakList":[
+    {
+      "startYmdhm":"201909181100",
+      "endYmdhm":"201909181130"
+    }
+  ],
+  "outWorkList":[
+    {
+      "startYmdhm":"201909181000",
+      "endYmdhm":"201909181330"
+    }
+  ]
+}
+
+/* 2회체크, 출근 예시 */
+POST https://workplace.apigw.ntruss.com/timeclock/apigw/v1/company/f3cf9659-782c-4508-bef9-f34c93f446df/timeclock/check
+HHOST: workplace.apigw.ntruss.com
+ Content-Type: application/json
+ x-ncp-apigw-timestamp: 1505290625682
+ x-ncp-apigw-api-key: DUxjP4HYTP8V7khBkgywdDHQjKuaYWGrppHKpvdv
+ x-ncp-iam-access-key: D78BB444D6D3C84CA38A
+ x-ncp-apigw-signature-v1: WTPItrmMIfLUk/UyUIyoQbA/z5hq9o3G8eQMolUzTEo=
 {
   "locale": "ko_KR",
   "commuteType": "on",
-  "companyId": "e065a8c6-2ab5-41a7-a4de-bf70c9a08142",
-  "emailId": "stopdragon@ravnus.com",
-  "basicYmd": "20231005",
-  "onYmdhm": "202310050855"
+  "companyId": "fe5f8382-aae3-4746-8d6e-f9d0545c5a9b",
+  "emailId": "green@naver.com",
+  "empExternalKey": "",
+  "basicYmd": "20190918",
+  "onYmdhm": "201909180855"
 }
 
-# 퇴근
-POST https://workplace.apigw.ntruss.com/timeclock/apigw/v1/company/e065a8c6-2ab5-41a7-a4de-bf70c9a08142/timeclock/check
+/* 2회체크, 퇴근 예시 */
+POST https://workplace.apigw.ntruss.com/timeclock/apigw/v1/company/f3cf9659-782c-4508-bef9-f34c93f446df/timeclock/check
 HHOST: workplace.apigw.ntruss.com
  Content-Type: application/json
- x-ncp-apigw-timestamp: datetime.datetime.now()
+ x-ncp-apigw-timestamp: 1505290625682
  x-ncp-apigw-api-key: DUxjP4HYTP8V7khBkgywdDHQjKuaYWGrppHKpvdv
  x-ncp-iam-access-key: D78BB444D6D3C84CA38A
  x-ncp-apigw-signature-v2: WTPItrmMIfLUk/UyUIyoQbA/z5hq9o3G8eQMolUzTEo=
 {
   "locale": "ko_KR",
   "commuteType": "off",
-  "companyId": "e065a8c6-2ab5-41a7-a4de-bf70c9a08142",
+  "companyId": "fe5f8382-aae3-4746-8d6e-f9d0545c5a9b",
   "emailId": "green@naver.com",
   "empExternalKey": "",
   "basicYmd": "20190918",
   "offYmdhm": "201909181803"
 }
-
-
