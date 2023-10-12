53d50e8f0f5180b5a6080b8b7e1dc57548f082a4
stopdragon <jiyong210@gmail.com>
2023-10-12 11:14:00 +0900

    complete work check

--- leave_work.py
+++ leave_work.py
@@ -1,42 +1,47 @@
-import sys, os, hashlib, hmac ,base64, requests, time
-from dotenv import load_dotenv
+import time, requests, hashlib, hmac, base64, os, sys
 
+from dotenv import load_dotenv
 load_dotenv()
 
-timestamp = int(time.time() * 1000)
-timestamp = str(timestamp)
+def make_signature():
+    nl = '\n'
 
-def	make_signature():
-  access_key = os.environ.get("access_key")
-  secret_key = os.environ.get("secret_key")
-  secret_key = bytes(secret_key, 'UTF-8')
-  method = "POST"
-  uri = "https://workplace.apigw.ntruss.com/timeclock/apigw/v1/company/e065a8c6-2ab5-41a7-a4de-bf70c9a08142/timeclock/check"
+    timestamp = str(int(time.time() * 1000))
+    access_key = os.environ.get("access_key")
+    secret_key = os.environ.get("secret_key")
+    method = "POST"
+    uri = "/timeclock/apigw/v1/company/e065a8c6-2ab5-41a7-a4de-bf70c9a08142/timeclock/check"
 
-  message = method + " " + uri + "\n" + timestamp + "\n" + access_key
-  message = bytes(message, 'UTF-8')
-  signingKey = base64.b64encode(hmac.new(secret_key, message, digestmod=hashlib.sha256).digest())
-  return signingKey
+    signature = f"{method} {uri}{nl}{timestamp}{nl}{access_key}"
 
-def	leave_work():  
-  uri = "https://workplace.apigw.ntruss.com/timeclock/apigw/v1/company/e065a8c6-2ab5-41a7-a4de-bf70c9a08142/timeclock/check"
-  http_header = {
-    'Content-Type': 'application/json',
+    signature_bytes = signature.encode('utf-8')
+    secret_key_bytes = secret_key.encode('utf-8')
+    hashed = hmac.new(secret_key_bytes, signature_bytes, hashlib.sha256).digest()
+    signature_base64 = base64.b64encode(hashed).decode('utf-8')
+
+    return timestamp, access_key, signature_base64
+
+timestamp, access_key, signature = make_signature()
+
+url = 'https://workplace.apigw.ntruss.com/timeclock/apigw/v1/company/e065a8c6-2ab5-41a7-a4de-bf70c9a08142/timeclock/check'
+headers = {
     'x-ncp-apigw-api-key': os.environ.get("API-key"),
     'x-ncp-apigw-timestamp': timestamp,
-    'x-ncp-iam-access-key': os.environ.get("access_key"),
-    'x-ncp-apigw-signature-v2': make_signature(),
-    }
-  content = {
+    'x-ncp-iam-access-key': access_key,
+    'x-ncp-apigw-signature-v2': signature,
+    'Content-Type': 'application/json',
+}
+
+data = {
     "locale": "ko_KR",
-    "commuteType": "off",
+    "commuteType": "on",
     "companyId": "e065a8c6-2ab5-41a7-a4de-bf70c9a08142",
     "emailId": "stopdragon@ravnus.com",
-    "basicYmd": "20231006",
-    "onYmdhm": "202310062139"  
-  }
-  response = requests.post(uri, headers=http_header, data=content)
+    "basicYmd": "20231012",
+    "onYmdhm": "202310120906"
+}
 
-  print(response.json())
-  print("response: ", response)
-  print("response.text: ", response.text)
+response = requests.post(url, headers=headers, json=data)
+
+print(response.status_code)
+print(response.text)
